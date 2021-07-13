04bb58c1e2cacce4de51f9fbd0cb0f3beb812680
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-13 14:48:53 -0500

    Create html container and first module with properties to start populating

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,9 @@
 import '../css/style.css';
-console.log("Hello World!");
+const tasks = [
+  { description: 'eat', completed: false, index: 0 },
+  { description: 'sleep', completed: false, index: 1 },
+  { description: 'code', completed: false, index: 2 },
+];
+
+import { metPopulator } from './populator';
+metPopulator.init();
---
+++ src/assets/js/populator.js
@@ -0,0 +1,12 @@
+const propPopulator = {
+  container: document.getElementsByTagName('ul'),
+  descr: localStorage.getItem('descr'),
+  status: localStorage.getItem('status'),
+  index: localStorage.getItem('index'),
+}
+
+export const metPopulator = {
+  fill(obj) {
+    
+  }
+}
