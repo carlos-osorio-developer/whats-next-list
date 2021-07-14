ac0d6719f54f954bcf2906e80be2013647ead71d
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-14 14:57:51 -0500

    Add dropOut method to change index value of objects

--- src/assets/js/dragndrop.js
+++ src/assets/js/dragndrop.js
@@ -1,19 +1,36 @@
+import tasks from './tasks'
+import metPopulator from './populator';
+
 const propDrag = {
   items: document.getElementsByTagName('li'),
+  container: document.getElementsByTagName('ul')[0],
 };
 
 const metDrag = {
   init() {
     for (let i = 0; i < propDrag.items.length; i += 1) {
       propDrag.items[i].addEventListener('dragstart', () => {propDrag.items[i].classList.add('ontop');}); 
-      propDrag.items[i].addEventListener('drop', () => {metDrag.dropOut(propDrag.items[i]);}); 
+      propDrag.items[i].addEventListener('drop', () => {metDrag.dropOut(i);}); 
       propDrag.items[i].addEventListener('dragover', (e) => {metDrag.onBottom(e, propDrag.items[i])});
       propDrag.items[i].addEventListener('dragleave', () => {propDrag.items[i].classList.remove('onbottom');});
     }    
   },
 
-  dropOut(item) {
-    console.log(item)
+  dropOut(newIndex) {
+    const dragged = document.getElementsByClassName('ontop')[0];
+    const hovered = document.getElementsByClassName('onbottom')[0];
+    const oldIndex = Array.prototype.indexOf.call(propDrag.container.children, dragged);
+    let newDict = metPopulator.sortStorage();    
+    const draging = newDict.splice(oldIndex, 1);
+    newDict.splice(newIndex, 0, draging[0]);
+    localStorage.clear();    
+    for (let i = 0; i < newDict.length; i += 1) {      
+      const obj = {description: newDict[i][0], status: newDict[i][1], index: newDict[i][2]}
+      metPopulator.fill(obj);      
+    }
+    dragged.classList.remove('ontop');
+    hovered.classList.remove('onbottom');
+    metPopulator.updateDOM();
   },
 
   onBottom(e,item) {
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,12 +2,7 @@ import '../css/style.scss';
 
 import metPopulator from './populator';
 import metDrag from './dragndrop';
-
-const tasks = [
-  { description: 'sleep', status: false, index: 2 },
-  { description: 'eat', status: false, index: 0 },
-  { description: 'code', status: false, index: 1 },
-];
+import tasks from './tasks'
 
 if (!localStorage.getItem('index')) {
   for (let i = 0; i < tasks.length; i += 1) {
