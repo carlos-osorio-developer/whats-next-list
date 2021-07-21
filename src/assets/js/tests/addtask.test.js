60ee981583b1b11c4444da391e6e76fdc111201a
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-21 14:23:39 -0500

    Add dependencies and testing file for add tasks

---
+++ src/assets/js/tests/addtask.test.js
@@ -0,0 +1,8 @@
+import metTasks from '../backend/addtask'
+
+describe('Testing addTasks functions', () => {
+
+  test('adds one element to the DOM', () => {
+    expect(functs.stringLength('Wharever')).toBe(8);
+  });  
+})
