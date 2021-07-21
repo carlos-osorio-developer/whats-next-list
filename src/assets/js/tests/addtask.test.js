bff031fb273314459c0e27de9852be2214b770df
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-21 16:43:00 -0500

    Add test for addtask method to check if DOM is being updated

--- src/assets/js/tests/addtask.test.js
+++ src/assets/js/tests/addtask.test.js
@@ -1,8 +1,21 @@
 import metTasks from '../backend/addtask'
 
-describe('Testing addTasks functions', () => {
+describe('Testing DOM elements', () => {
 
   test('adds one element to the DOM', () => {
-    expect(functs.stringLength('Wharever')).toBe(8);
-  });  
+    document.body.innerHTML = `    
+    <input id="name-field" value="Something">
+    <i class="far fa-calendar-plus"></i>
+    <ul></ul>
+    <div class="button"><p>Clear all completed</p></div>`;        
+
+    const addIcon = document.getElementsByClassName('fa-calendar-plus')[0];
+    addIcon.addEventListener('click', () => { metTasks.create(); });
+    
+    addIcon.click();
+
+    expect(document.getElementsByTagName('p')[0].textContent).toBe('Something');
+  });
+
+
 })
