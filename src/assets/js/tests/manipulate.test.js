73c1b91c78a2dacd5e8be413ba8e564301b7e6ba
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-22 11:52:18 -0500

    Fix linters

--- src/assets/js/tests/manipulate.test.js
+++ src/assets/js/tests/manipulate.test.js
@@ -1,6 +1,6 @@
-import metDrag from "../backend/dragndrop";
-import metName from "../backend/taskname";
-import metStatus from "../backend/status";
+import metDrag from '../backend/dragndrop';
+import metName from '../backend/taskname';
+import metStatus from '../backend/status';
 
 class LocalStorageMock {
   constructor() {
@@ -30,7 +30,7 @@ describe('Testing the drag and drop function', () => {
     localStorage.setItem('description', 'Task 1,Task 2,Task 3');
     localStorage.setItem('status', 'true,false,true');
     localStorage.setItem('index', '0,1,2');
-    
+
     document.body.innerHTML = `    
     <input id="name-field" placeholder="Add to your list..." value="Something" />
     <i class="far fa-calendar-plus" />
@@ -41,12 +41,10 @@ describe('Testing the drag and drop function', () => {
     </ul>
     <div class="button"><p>Clear all completed</p></div></body>`;
 
-    const items = document.getElementsByTagName('li');
-    
     metDrag.dropOut(0);
 
     expect(localStorage.getItem('description')).toBe('Task 3,Task 1,Task 2');
-  });  
+  });
 });
 
 describe('Testing the update name function', () => {
@@ -54,7 +52,7 @@ describe('Testing the update name function', () => {
     global.localStorage = new LocalStorageMock();
     localStorage.setItem('description', 'Task 1,Task 2,Task 3');
     localStorage.setItem('status', 'false,false,false');
-    localStorage.setItem('index', '0,1,2');    
+    localStorage.setItem('index', '0,1,2');
 
     document.body.innerHTML = `    
     <input id="name-field" placeholder="Add to your list..." value="Something" />
@@ -71,18 +69,18 @@ describe('Testing the update name function', () => {
     for (let i = 0; i < items.length; i += 1) {
       items[i].firstElementChild.lastElementChild.addEventListener('click', () => { metName.editName(i); });
     }
-    
+
     const firstPTag = items[0].firstElementChild.lastElementChild;
     firstPTag.click();
 
     expect([firstPTag.style.display, items[0].firstElementChild.lastElementChild.tagName]).toStrictEqual(['none', 'INPUT']);
-  });  
+  });
 
   test('update the name in the localStorage and DOM', () => {
     global.localStorage = new LocalStorageMock();
     localStorage.setItem('description', 'Task 1,Task 2,Task 3');
     localStorage.setItem('status', 'false,false,false');
-    localStorage.setItem('index', '0,1,2');    
+    localStorage.setItem('index', '0,1,2');
 
     document.body.innerHTML = `    
     <input id="name-field" placeholder="Add to your list..." value="Something" />
@@ -99,18 +97,18 @@ describe('Testing the update name function', () => {
     for (let i = 0; i < items.length; i += 1) {
       items[i].firstElementChild.lastElementChild.addEventListener('click', () => { metName.editName(i); });
     }
-    
+
     const firstPTag = items[0].firstElementChild.lastElementChild;
     firstPTag.click();
     const inputField = items[0].firstElementChild.lastElementChild;
     inputField.value = 'New Task';
     const dots = items[0].getElementsByClassName('fa-ellipsis-v')[0];
     const thrash = items[0].lastChild;
-    
+
     metName.updateName(firstPTag, inputField, 0, dots, thrash);
 
     expect(localStorage.getItem('description')).toBe('New Task,Task 2,Task 3');
-  });  
+  });
 });
 
 describe('Testing the update status function', () => {
@@ -118,7 +116,7 @@ describe('Testing the update status function', () => {
     global.localStorage = new LocalStorageMock();
     localStorage.setItem('description', 'Task 1,Task 2,Task 3');
     localStorage.setItem('status', 'false,false,false');
-    localStorage.setItem('index', '0,1,2');    
+    localStorage.setItem('index', '0,1,2');
 
     document.body.innerHTML = `    
     <input id="name-field" placeholder="Add to your list..." value="Something" />
@@ -130,13 +128,12 @@ describe('Testing the update status function', () => {
     </ul>
     <div class="button"><p>Clear all completed</p></div></body>`;
 
-    const items = document.getElementsByTagName('li');       
+    const items = document.getElementsByTagName('li');
     const firstCheckBox = items[0].firstElementChild.firstElementChild;
 
     firstCheckBox.checked = true;
-    console.log('this is it ',firstCheckBox.checked)
-    metStatus.updateStatus(firstCheckBox, 0);  
+    metStatus.updateStatus(firstCheckBox, 0);
 
     expect(localStorage.getItem('status')).toStrictEqual('true,false,false');
-  });  
+  });
 });
