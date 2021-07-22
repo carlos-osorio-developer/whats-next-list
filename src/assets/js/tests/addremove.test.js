0e7bf4f5160752235a39bd428b69d6aaa253614a
errea <erreakezy@gmail.com>
2021-07-22 01:19:25 +0100

    Add test for deletebutton checked box

--- src/assets/js/tests/addremove.test.js
+++ src/assets/js/tests/addremove.test.js
@@ -61,6 +61,30 @@ describe('Testing the add function', () => {
 
 describe('Testing the delete function', () => {
 
+  test('delete all checked elements', () => {
+
+    global.localStorage = new LocalStorageMock;
+    localStorage.setItem('status', 'true,false,true');
+    localStorage.setItem('index', '0,1,2');
+    localStorage.setItem('description', 'Task 1,Task 2,Task 3');
+
+    document.body.innerHTML = `    
+    <input id="name-field" placeholder="Add to your list..." value="Something" />
+    <i class="far fa-calendar-plus" />
+    <ul>
+    <li draggable="true"><nav><input class="status" checked name="completed" type="checkbox"><p>Task 1</p></nav><i class="fas fa-ellipsis-v"></i><i class="fas fa-trash-alt invisible"></i></li>
+    <li draggable="true"><nav><input class="status" name="completed" type="checkbox"><p>Task 2</p></nav><i class="fas fa-ellipsis-v"></i><i class="fas fa-trash-alt invisible"></i></li>
+    <li draggable="true"><nav><input class="status" checked name="completed" type="checkbox"><p>Task 3</p></nav><i class="fas fa-ellipsis-v"></i><i class="fas fa-trash-alt invisible"></i></li>
+    </ul>
+    <div class="button"><p>Clear all completed</p></div></body>`;        
+
+    const deletebutton = document.getElementsByClassName('button')[0];     
+        deletebutton.addEventListener('click', () => metDelete.deleteChecked );
+        deletebutton.click();
+  
+    expect(localStorage).toBe(1);
+  });
+
   test('delete one element to the DOM', () => {
 
     global.localStorage = new LocalStorageMock;
